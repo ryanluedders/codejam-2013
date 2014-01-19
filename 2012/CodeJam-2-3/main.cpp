c652293258c05edca96597b43ef5523045a53678
Ryan Luedders <ryan@ryanluedders.com>
2014-01-19 10:36:48 -0600

    organize into 2012/2013 folders. add 2012.

---
+++ 2012/CodeJam-2-3/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CodeJam-2-3
+//
+//  Created by Ryan Luedders on 5/5/12.
+//  Copyright (c) 2012 n/a. All rights reserved.
+//
+
+#include <iostream>
+#include "CodeJam-2-3.h"
+
+int main(int argc, const char * argv[])
+{
+    return CJ23::runSolution(argc, argv);
+}
+
